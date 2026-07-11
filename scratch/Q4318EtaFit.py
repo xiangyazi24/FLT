c5a8bef99970b1f9c62a1ec16749679912c01cee
xiangyazi24 <xiangyazi24@users.noreply.github.com>
2026-07-11 18:14:03 -0500

    Fix polynomial composition in X0(35) eta fitting

--- scratch/Q4318EtaFit.py
+++ scratch/Q4318EtaFit.py
@@ -1,7 +1,7 @@
 import sympy as sp
 x=sp.symbols('x')
 f=x**8-4*x**7-6*x**6-4*x**5-9*x**4+4*x**3-6*x**2+4*x+1
-N=80
+N=90
 
 def mul(A,B):
  C=[sp.Integer(0)]*(N+1)
@@ -33,7 +33,7 @@ def add(A,B):return [A[i]+B[i] for i in range(N+1)]
 def scale(A,c):return [c*z for z in A]
 def compose_poly(poly,A):
  B=[sp.Integer(0)]*(N+1)
- for coeff in reversed(sp.Poly(poly,x).all_coeffs()): B=add(mul(B,A),[coeff]+[0]*N)
+ for coeff in sp.Poly(poly,x).all_coeffs(): B=add(mul(B,A),[coeff]+[0]*N)
  return B
 def series_powers(A,maxd):
  out=[[sp.Integer(0)]*(N+1) for _ in range(maxd+1)];out[0][0]=1
@@ -47,9 +47,10 @@ xx=shift(mul(mul(E1,E35),mul(inv(E5),inv(E7))),1)
 fx=compose_poly(f,xx)
 y=[sp.Integer(0)]*(N+1);y[0]=1
 for n in range(1,N+1): y[n]=(fx[n]-sum(y[k]*y[n-k] for k in range(1,n)))/2
-xp=series_powers(xx,16)
+print('x first',xx[:12]);print('y first',y[:12])
+xp=series_powers(xx,18)
 
-def fit(T,maxd=9):
+def fit(T,maxd=11):
  for dR in range(maxd+1):
   for dP in range(maxd+1):
    for dQ in range(maxd+1):
@@ -57,7 +58,7 @@ def fit(T,maxd=9):
     for i in range(dR+1): cols.append(mul(T,xp[i]));names.append(('R',i))
     for i in range(dP+1): cols.append(scale(xp[i],-1));names.append(('P',i))
     for i in range(dQ+1): cols.append(scale(mul(y,xp[i]),-1));names.append(('Q',i))
-    rows=min(N+1,len(cols)+15)
+    rows=min(N+1,len(cols)+18)
     ns=sp.Matrix([[cols[j][i] for j in range(len(cols))] for i in range(rows)]).nullspace()
     for v in ns:
      if not any(v[i]!=0 for i,nm in enumerate(names) if nm[0]=='R'): continue
@@ -69,18 +70,13 @@ def fit(T,maxd=9):
       g=sp.gcd(sp.gcd(sp.Poly(polys['R'],x),sp.Poly(polys['P'],x)),sp.Poly(polys['Q'],x)).as_expr()
       for k in polys:polys[k]=sp.factor(polys[k]/g)
       print('degrees',dR,dP,dQ)
-      print('R=',polys['R'])
-      print('P=',polys['P'])
-      print('Q=',polys['Q'])
-      print('formula=(P+y*Q)/R')
+      print('R=',polys['R']);print('P=',polys['P']);print('Q=',polys['Q'])
       return polys
  raise RuntimeError('no fit')
 print('A_FORMULA'); pa=fit(a)
 print('H_FORMULA'); ph=fit(h)
-# Exact algebraic certificates in Q[x,y]/(y^2-f): substitute formulas into fiber product.
 yv=sp.symbols('y')
-AA=(pa['P']+yv*pa['Q'])/pa['R']
-HH=(ph['P']+yv*ph['Q'])/ph['R']
+AA=(pa['P']+yv*pa['Q'])/pa['R']; HH=(ph['P']+yv*ph['Q'])/ph['R']
 F=HH*(AA**2+13*AA+49)*(AA**2+5*AA+1)**3-AA*(HH**2+10*HH+5)**3
 num=sp.together(F).as_numer_denom()[0]
 red=sp.Poly(num,yv).rem(sp.Poly(yv**2-f,yv)).as_expr()
