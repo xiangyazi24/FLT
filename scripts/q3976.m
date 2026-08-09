81437c5d47e5bff22298bfbfd02eb93d5cc72a52
xiangyazi24 <xiangyazi24@users.noreply.github.com>
2026-08-09 02:15:08 -0400

    chore: compute exact degree-10 branch divisors

--- scripts/q3976.m
+++ scripts/q3976.m
@@ -1,8 +1,9 @@
 SetColumns(0);
 Q := Rationals();
 
-// Tate blow-up b = c + c^2 v, viewed as a degree-10 cover of the v-line.
-Kv<v> := FunctionField(Q);
+// Tate blow-up b = c + c^2 v, as a degree-10 polynomial in c.
+Rv<v> := PolynomialRing(Q);
+Kv := FieldOfFractions(Rv);
 Pc<c> := PolynomialRing(Kv);
 bb := c+c^2*v;
 F5 := bb-c;
@@ -14,19 +15,24 @@ G11 := F7*F5^3-bb*c*F6^3;
 G12 := c*F6*(F5^2*F8+F7^2);
 G13 := F5*F7^3+bb*c*F6^3*F8;
 G14 := F7*(bb*F6^2*F9-c^2*F5*F8^2);
-num25 := G11*G13^3-bb*G14*G12^3;
-assert IsDivisibleBy(num25,F5);
-sub25 := ExactQuotient(num25,F5);
-assert IsDivisibleBy(sub25,c^40);
+sub25 := ExactQuotient(G11*G13^3-bb*G14*G12^3,F5);
 E := ExactQuotient(sub25,c^40);
-fT := E/LeadingCoefficient(E);
-print "TATE_POLY_DEGREE_TERMS", Degree(E), #Terms(E);
-KT<ct> := FunctionField(fT);
-print "TATE_FIELD_DEGREE", Degree(KT);
-print "TATE_GENUS", Genus(KT);
+print "TATE_E_DEGREE", Degree(E);
+print "BEGIN_TATE_DISC";
+dE := Discriminant(E);
+numE := Numerator(dE);
+denE := Denominator(dE);
+print "TATE_DISC_NUM_DEGREE_TERMS", Degree(numE), #Terms(numE);
+print "TATE_DISC_DEN", denE;
+facE := Factorization(numE);
+print "TATE_DISC_FACTORS_COUNT", #facE;
+for fe in facE do
+  print "TATE_FACTOR", Degree(fe[1]), fe[2], fe[1];
+end for;
 
-// LMFDB H(C,W,1), viewed as a degree-10 cover of the C-line.
-KC<C> := FunctionField(Q);
+// LMFDB H(C,W,1), degree 10 in W over Q(C).
+RC<C> := PolynomialRing(Q);
+KC := FieldOfFractions(RC);
 PW<W> := PolynomialRing(KC);
 H :=
  C^3*W^8+2*C^2*W^9+C*W^10
@@ -37,9 +43,16 @@ H :=
  -2*C^4*W^2-C^3*W^3+2*C^2*W^4-C*W^5
  +C^4*W+2*C^3*W^2-2*C^2*W^3+C*W^4
  -C^3*W+2*C^2*W^2+C^3;
-fL := H/LeadingCoefficient(H);
-print "LMFDB_POLY_DEGREE_TERMS", Degree(H), #Terms(H);
-KL<wl> := FunctionField(fL);
-print "LMFDB_FIELD_DEGREE", Degree(KL);
-print "LMFDB_GENUS", Genus(KL);
-print "DEGREE10_FIELDS_COMPLETE";
+print "LMFDB_H_DEGREE", Degree(H);
+print "BEGIN_LMFDB_DISC";
+dH := Discriminant(H);
+numH := Numerator(dH);
+denH := Denominator(dH);
+print "LMFDB_DISC_NUM_DEGREE_TERMS", Degree(numH), #Terms(numH);
+print "LMFDB_DISC_DEN", denH;
+facH := Factorization(numH);
+print "LMFDB_DISC_FACTORS_COUNT", #facH;
+for fh in facH do
+  print "LMFDB_FACTOR", Degree(fh[1]), fh[2], fh[1];
+end for;
+print "BRANCH_DIVISORS_COMPLETE";
