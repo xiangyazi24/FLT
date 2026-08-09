f0d1cadca9532fb59acab44d374d71538fde748e
xiangyazi24 <xiangyazi24@users.noreply.github.com>
2026-08-09 02:24:45 -0400

    chore: compute Tate holomorphic differentials over F11

--- scripts/q3976.m
+++ scripts/q3976.m
@@ -1,8 +1,7 @@
 SetColumns(0);
-k := GF(101);
-
-// Tate blow-up model E(c,v)=0 over F_101.
-A2<c,v> := AffineSpace(k,2);
+k := GF(11);
+Kv<v> := FunctionField(k);
+Pc<c> := PolynomialRing(Kv);
 b := c+c^2*v;
 F5 := b-c;
 F6 := b-c-c^2;
@@ -15,31 +14,13 @@ G13 := F5*F7^3+b*c*F6^3*F8;
 G14 := F7*(b*F6^2*F9-c^2*F5*F8^2);
 sub25 := ExactQuotient(G11*G13^3-b*G14*G12^3,F5);
 E := ExactQuotient(sub25,c^40);
-CTaff := Curve(A2,E);
-CT := ProjectiveClosure(CTaff);
-print "TATE_PROJECTIVE_DEGREE", Degree(CT);
-
-// Official LMFDB plane curve.
-P2<C,W,S> := ProjectiveSpace(k,2);
-H :=
- C^3*W^8+2*C^2*W^9+C*W^10
- +C^4*W^6*S+3*C^3*W^7*S+2*C^2*W^8*S
- -C^4*W^5*S^2-2*C^3*W^6*S^2+C*W^8*S^2
- -C^5*W^3*S^3-3*C^4*W^4*S^3+C^3*W^5*S^3+2*C^2*W^6*S^3-2*C*W^7*S^3-W^8*S^3
- +C^4*W^3*S^4-C^3*W^4*S^4-4*C^2*W^5*S^4-C*W^6*S^4
- -2*C^4*W^2*S^5-C^3*W^3*S^5+2*C^2*W^4*S^5-C*W^5*S^5
- +C^4*W*S^6+2*C^3*W^2*S^6-2*C^2*W^3*S^6+C*W^4*S^6
- -C^3*W*S^7+2*C^2*W^2*S^7+C^3*S^8;
-CL := Curve(P2,H);
-print "LMFDB_PROJECTIVE_DEGREE", Degree(CL);
-print "BEGIN_ISOMORPHISM";
-ok, phi := IsIsomorphic(CT,CL);
-print "ISOMORPHIC", ok;
-if ok then
-  print "FORWARD_MAP";
-  print phi;
-  psi := Inverse(phi);
-  print "INVERSE_MAP";
-  print psi;
-end if;
-print "FINITE_FIELD_ISOMORPHISM_COMPLETE";
+print "TATE_E_DEGREE", Degree(E);
+FT<ct> := FunctionField(E/LeadingCoefficient(E));
+print "TATE_FF_DEGREE", Degree(FT);
+print "BEGIN_HOLO_DIFF";
+B := BasisOfHolomorphicDifferentials(FT);
+print "HOLO_DIM", #B;
+for i in [1..#B] do
+  print "OMEGA", i, B[i];
+end for;
+print "TATE_HOLO_COMPLETE";
